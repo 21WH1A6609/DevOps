137f31f4851039acb25267e58bf011480a7f2459
21WH1A6609 <96616271+21WH1A6609@users.noreply.github.com>
2024-06-12 09:47:48 +0530

    Update prog.java

--- prog.java
+++ prog.java
@@ -4,6 +4,7 @@ public static void main(String[]  args){
 System.out.println("Hello Perseus.");
 System.out.println("Welcome, Son of Poseidon.");
 System.out.println("This is Camp Half-Blood");
+System.out.println("Son of the Ocean");
 }
 }
 
