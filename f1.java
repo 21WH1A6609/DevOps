52a2748d330f43919d551f0347d5b76e0844ae39
21WH1A6609 <96616271+21WH1A6609@users.noreply.github.com>
2024-06-12 09:56:56 +0530

    Add files via upload

---
+++ f1.java
@@ -0,0 +1,5 @@
+import java.util.*;
+class f1{
+public static void main(String[] args){
+System.out.println("Hello FRIDAY");}
+}
---
+++ f2.java
@@ -0,0 +1,5 @@
+import java.util.*;
+class f2{
+public static void main(String[] args){
+System.out.println("Hello, love");}
+}
